bf10853187c591404415a405926d211eb6f01060
Shayan Sen <senshayan2103@gmail.com>
2024-11-26 21:43:27 +0530

    Checking Ends

--- src/main/java/payroll/OrderController.java
+++ src/main/java/payroll/OrderController.java
@@ -100,5 +100,4 @@ class OrderController {
                 .body(Problem.create() //
                         .withTitle("Method not allowed") //
                         .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
-    }
-}
+    }}
